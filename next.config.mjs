000ccec0c053523cafbb806ad6952c01f11a03fc
Jökull Sólberg <jokull@solberg.is>
2024-05-22 09:47:33 +0000

    skipLibCheck

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compilerOptions: {
+    skipLibCheck: true,
+    noEmit: true,
+  },
   experimental: {
     ppr: true,
   },
